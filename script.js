3010ab83b7f9bc916664f4a66a5ede04e239ebed
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 18:04:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	// for (let char of str) {
+ //        if (str.indexOf(char) === str.lastIndexOf(char)) {
+ //            return char;
+ //        }
+ //    }
+
+	let count = {};
 	for (let char of str) {
-        if (str.indexOf(char) === str.lastIndexOf(char)) {
-            return char;
-        }
-    }
+		count[char] = (count[char] || 0 ) +1;
+	}
+	for (let char of str) {
+		if(count[char] === 1){
+			return char;
+		}
+	}
     return null;
 }
 const input = prompt("Enter a string");
