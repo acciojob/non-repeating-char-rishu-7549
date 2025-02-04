a82e0dc11d4dfc653ff1ead1d0044a9aab0da70a
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-04 17:59:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let char of str) {
+        if (str.indexOf(char) === str.lastIndexOf(char)) {
+            return char;
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
